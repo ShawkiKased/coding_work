b136d82b122121975e059f10cd101802b56ae220
Shawki <shawksta64@gmail.com>
2020-05-08 14:29:30 -0400

    challenge 3

--- challenge_3.js
+++ challenge_3.js
@@ -1,9 +1,13 @@
 //3. Given a number return the sum of all the multiples of 3 or 5 below the given number.
 
 function multiple_sum(num) {
-
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) sum += i;
+    }
+    return sum;
 }
 
 //test cases:
-multiple_sum(10) // Output: 23
-multiple_sum(20) // Output: 78
+console.log(multiple_sum(10)) // Output: 23
+console.log(multiple_sum(20)) // Output: 78
