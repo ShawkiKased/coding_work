59ddd09760b8e066fd5ab6909bcd38cab7dc2d43
Shawki <shawksta64@gmail.com>
2020-05-08 14:13:42 -0400

    Challenge 1

--- challenge_1.js
+++ challenge_1.js
@@ -1,11 +1,12 @@
 //1. Create a function that takes any non-negative integer as an argument and return it with its 
 //digits in descending order.
 function decending_num(num){
-
+    let digits = (num + "").split("");
+    let newDigits = digits.sort(function (a, b) { return b - a });
+    return newDigits.join("");
 }
 
-
 //test cases:
-decending_num(21445) //Output: 54421
-decending_num(145263) //Output: 654321
-decending_num(123456789) //Output: 98765432
+console.log(decending_num(21445)) //Output: 54421
+console.log(decending_num(145263)) //Output: 654321
+console.log(decending_num(123456789)) //Output: 98765432
