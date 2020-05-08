8debbb83077e5d3d8cbbb86d6663a16bc5ba446a
Shawki <shawksta64@gmail.com>
2020-05-08 14:24:04 -0400

    Challenge_2

--- challenge_2.js
+++ challenge_2.js
@@ -1,11 +1,16 @@
 //2.Given a string of words, return the length of the shortest word(s).The given data will 
 //always be a string and will never be empty.
 
-function short_count(words) {
-
+function short_count(str) {
+    let words = str.split(" ");
+    let min = words[0].length;
+    for (i = 0; i < words.length; i++) {
+        if (words[i].length < min) min = words[i].length;
+    }
+    return min;
 }
 
 //test cases:
-short_count('Test, this is a test') // Output: 1
-short_count('The dog’s mother was happy') // Output: 3
-short_count('Extraordinarily appetizing pineapple!') // Output: 9
+console.log(short_count('Test, this is a test')) // Output: 1
+console.log(short_count('The dog’s mother was happy')) // Output: 3
+console.log(short_count('Extraordinarily appetizing pineapple!')) // Output: 9
