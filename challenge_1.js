ec53135367cfd79dba653f8775c61407e0131264
Shawki <shawksta64@gmail.com>
2020-05-08 15:52:51 -0400

    Explanation for Challenge1

--- challenge_1.js
+++ challenge_1.js
@@ -1,8 +1,14 @@
 //1. Create a function that takes any non-negative integer as an argument and return it with its 
 //digits in descending order.
-function decending_num(num){
+
+
+function descending_num(num){
+    //We want to first turn the number into an array of seperate digits, we add "" to the num to be able to split.
     let digits = (num + "").split("");
+    //We then want to use the sort function, by using funcion (a, b) {return b - a} it will use the compare function
+    //And return the numbers in descending order.
     let newDigits = digits.sort(function (a, b) { return b - a });
+    //Finally, we return the numbers back into number form by joining.
     return newDigits.join("");
 }
 
